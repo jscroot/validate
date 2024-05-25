ee3a7a19a44d24529e9f816445b5211d47b3f201
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-25 08:47:02 +0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # validate
 Validate Input
+
+## Release
+
+Tag version
+
+```sh
+git tag                                  #check current version
+git tag v0.0.1                           #set tag version
+git push origin --tags  
+```
---
+++ croot.js
@@ -0,0 +1,16 @@
+// Fungsi untuk memproses input
+export function validateUserName(input) {
+    input.value = input.value.toLowerCase(); // Convert to lowercase
+    input.value = input.value.replace(/\s+/g, ''); // Remove spaces
+    input.value = input.value.replace(/[^a-z0-9_-]/gi, ''); // Remove special characters except _ and -
+}
+
+// Fungsi validasi nomor telepon
+export function validatePhoneNumber(input) {
+    // Hanya izinkan angka
+    input.value = input.value.replace(/[^0-9]/g, '');
+    // Jika karakter pertama adalah 0, hapus karakter tersebut
+    if (input.value.length > 1 && input.value.charAt(0) === '0') {
+        input.value = input.value.substr(1);
+    }
+}
